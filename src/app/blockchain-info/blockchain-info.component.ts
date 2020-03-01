92d2763abb96a7eeae79f9b550a3422bc40073d5
zhaizhonghao <390930230@qq.com>
2020-03-01 22:08:34 +0800

    add front end of transactions

--- src/app/blockchain-info/blockchain-info.component.ts
+++ src/app/blockchain-info/blockchain-info.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockchainInfoComponent implements OnInit {
 
+  transactions = [
+    {
+      "txId": "0ccdd561e3760ee3fe934b505064cc1db6adf992e8916ed012f9e100ef261ecd",
+      "channelId": "airlinechannel",
+      "timestamp": "2020-02-02T14:05:18.584Z",
+      "type": "ENDORSER_TRANSACTION"
+    },
+    {
+      "txId": "0ccdd561e3760ee3fe934b505064cc1db6adf992e8916ed012f9e100ef261ecd",
+      "channelId": "airlinechannel",
+      "timestamp": "2020-02-02T14:05:18.584Z",
+      "type": "ENDORSER_TRANSACTION"
+    },
+    {
+      "txId": "0ccdd561e3760ee3fe934b505064cc1db6adf992e8916ed012f9e100ef261ecd",
+      "channelId": "airlinechannel",
+      "timestamp": "2020-02-02T14:05:18.584Z",
+      "type": "ENDORSER_TRANSACTION"
+    },
+    {
+      "txId": "0ccdd561e3760ee3fe934b505064cc1db6adf992e8916ed012f9e100ef261ecd",
+      "channelId": "airlinechannel",
+      "timestamp": "2020-02-02T14:05:18.584Z",
+      "type": "ENDORSER_TRANSACTION"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  displayedColumnsForChaincode: string[] = ['txId','channelId','timestamp', 'type'];
 }
